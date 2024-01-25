91f4202d869453f22cd11242a89a4e68c5453964
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-25 10:23:20 -0500

    Add TodoList

---
+++ lib/todo_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add-todo');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
